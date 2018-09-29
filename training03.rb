95cfba30feb31f19be2534124d9bd89b47862a55
Natdenice <Clad1987@hotmail.com>
2018-09-29 15:24:27 +0200

    Ajout du fichier training03.rb/ tranformer nombre pos en neg , nombre neg en pos

---
+++ training03.rb
@@ -0,0 +1,2 @@
+print -300.abs # utiliser la methode abs ( absolue ) permet de renvoyer un nombre négatif en nombre positif
+print 300*-1  # Multiplier un positif par -1 permet de passer le positif en négatif
